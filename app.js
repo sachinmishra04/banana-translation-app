0557587ca3c93568a0fecffc826be0b78f4d2bd8
sachinmishra <sachinmishra0422@gmail.com>
2021-07-14 09:04:58 +0530

    add an event listener

---
+++ app.js
@@ -0,0 +1,23 @@
+/*
+ * Project:  banana-translation-app
+ * File:      app.js
+ * Author:   Amit Mishra (amit.mishra+CODA@gmail.com)
+ * 
+ * Description:
+ * 
+ * Revision History:
+ *   2021-July-14: 
+ * 
+ * Copyright (c) 2021 Self
+ * 
+ * License:
+ *    
+ */
+
+const translateBtn=document.querySelector(".translate-btn");
+
+console.log(translateBtn)
+
+translateBtn.addEventListener(onclick, function show(){
+    alert("hi ji")
+});
