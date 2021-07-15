d3d6e98d9992babefabc5039b527cff7c998f877
sachinmishra <sachinmishra0422@gmail.com>
2021-07-15 13:54:54 +0530

    complete functional app

--- app.js
+++ app.js
@@ -2,24 +2,30 @@
  * Project:  banana-translation-app
  * File:      app.js
  * Author:   Amit Mishra (amit.mishra+CODA@gmail.com)
- * 
+ *
  * Description:
- * 
+ *
  * Revision History:
- *   2021-July-14: 
- * 
+ *   2021-July-14:
+ *
  * Copyright (c) 2021 Self
- * 
+ *
  * License:
- *    
+ *
  */
 
-const translateBtn=document.querySelector(".translate-btn");
-const textInput=document.querySelector(".textInput");
-console.log(translateBtn)
+const translateBtn = document.querySelector(".translate-btn");
+const textInput = document.querySelector(".textInput");
+const outputTxt = document.querySelector(".outputTxt");
 
-translateBtn.addEventListener("click", function show(){
-    alert("hi ji")
+translateBtn.addEventListener("click", () => {
+  console.log(textInput.value);
+  outputTxt.innerText = "";
+  fetch(
+    `https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json?text=${textInput.value}`
+  )
+    .then((res) => res.json())
+    .then((output) => (outputTxt.innerText = output.contents.translation));
 });
 
-textInput.addEventListener(onchange,()=>console.log("hi"))
+textInput.addEventListener(onchange, () => console.log("hi"));
